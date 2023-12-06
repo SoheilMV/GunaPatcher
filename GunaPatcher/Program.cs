bf0a5f3a209f8c15da45583e00be3a5482a368ff
SoheilMV <Soheil.8221@gmail.com>
2023-12-06 07:17:20 +0330

    Finds the class more accurately

--- GunaPatcher/Program.cs
+++ GunaPatcher/Program.cs
@@ -1,57 +1,130 @@
 ﻿using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using OpCodes = dnlib.DotNet.Emit.OpCodes;
 
-namespace GunapPatcher
+namespace GunaPatcher
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-			var module = ModuleDefMD.Load(args[0]);
-			var newLocation = module.Location.Replace(module.Name, "Cracked");
-
-			if (module.Name != "Guna.UI2" && module.Assembly.Name != "Guna.UI2" && module.Name != "Guna.Charts.WinForms.dll" && module.Assembly.Name != "Guna.Charts.WinForms")
-				Console.WriteLine("Are you doing this on purpose ?");
-			else
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "GunaPatcher";
+			try
 			{
-				bool cracked = false;
-				Console.WriteLine($"[Loaded] {module.Assembly.Name} | {module.Assembly.Version}");
-				foreach (TypeDef type in module.Types)
+                ModuleDefMD module = null;
+                if (args.Length > 0)
 				{
-					foreach (TypeDef nt2 in type.NestedTypes.Where((TypeDef nt) => nt.HasMethods && nt.Methods.Count() == 5 && nt.Fields.Count() == 5))
+					module = ModuleDefMD.Load(args[0], ModuleDefMD.CreateModuleContext());
+				}
+				else
+				{
+					Console.Write("Path: ");
+					string path = Console.ReadLine();
+					Console.WriteLine();
+					if (File.Exists(path))
 					{
-						foreach (MethodDef method in nt2.Methods.Where((MethodDef m) => m.HasBody && m.Body.HasInstructions && m.Body.Instructions.Count() > 300))
-						{
-							method.Body.Variables.Clear();
-							method.Body.Instructions.Clear();
-							method.Body.ExceptionHandlers.Clear();
-							method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, "Cracked by TheHellTower"));
-							method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, "https://t.me/TheHellTower"));
-							method.Body.Instructions.Add(Instruction.Create(OpCodes.Call, module.Import(typeof(MessageBox).GetMethod("Show", new Type[] { typeof(string), typeof(string) }))));
-							method.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
-
-							cracked = true;
-
-							Console.WriteLine($"{method.Name} Cracked!");
-						}
+						module = ModuleDefMD.Load(path, ModuleDefMD.CreateModuleContext());
 					}
 				}
 
-				if (cracked)
+				var newLocation = module.Location.Replace(module.Name, "Cracked");
+
+				if (module.Name != "Guna.UI2" && module.Assembly.Name != "Guna.UI2" && module.Name != "Guna.Charts.WinForms.dll" && module.Assembly.Name != "Guna.Charts.WinForms")
+					Console.WriteLine("Are you doing this on purpose ?");
+				else
 				{
-					if (!Directory.Exists(newLocation))
-						Directory.CreateDirectory(newLocation);
+					GunaStatus status = GunaStatus.None;
+					Console.WriteLine($"[Loaded] {module.Assembly.Name} | {module.Assembly.Version}");
+					foreach (TypeDef type in module.Types.Where((TypeDef t) => t.HasNestedTypes))
+					{
+						foreach (TypeDef nestedType in type.NestedTypes.Where((TypeDef nt) => nt.HasMethods && nt.HasFields))
+						{
+							if ((CheckMethodsTypeSigCount(nestedType.Methods, module.CorLibTypes.Int32) == 1 &&
+								CheckMethodsTypeSigCount(nestedType.Methods, module.CorLibTypes.Boolean) == 1 &&
+								CheckMethodsTypeSigCount(nestedType.Methods, module.ImportAsTypeSig(typeof(Dictionary<string, string>))) == 1 &&
+								CheckMethodsTypeSigCount(nestedType.Methods, module.CorLibTypes.Void) == 2) &&
+								(CheckFieldsTypeSigCount(nestedType.Fields, module.CorLibTypes.Boolean) == 1 &&
+								CheckFieldsTypeSigCount(nestedType.Fields, type) == 1 &&
+								CheckFieldsTypeSigCount(nestedType.Fields, module.ImportAsTypeSig(typeof(Func<Dictionary<string, string>>))) == 1 &&
+								 CheckFieldsTypeSigCount(nestedType.Fields, module.ImportAsTypeSig(typeof(Func<bool>))) == 1 &&
+								 CheckFieldsTypeSigCount(nestedType.Fields, module.ImportAsTypeSig(typeof(Func<int>))) == 1))
+							{
+								foreach (MethodDef method in nestedType.Methods.Where((MethodDef m) => m.HasBody && m.Body.HasVariables && !m.HasReturnType))
+								{
+									method.Body.Variables.Clear();
+									method.Body.Instructions.Clear();
+									method.Body.ExceptionHandlers.Clear();
+									method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, "Cracked by TheHellTower | Soheil MV"));
+									method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr, "Telegram : @TheHellTower | @MVSoft_ir"));
+									method.Body.Instructions.Add(Instruction.Create(OpCodes.Call, module.Import(typeof(MessageBox).GetMethod("Show", new Type[] { typeof(string), typeof(string) }))));
+									method.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
 
-					module.Write($"{newLocation}\\{module.Name}");
+									status = GunaStatus.Cracked;
 
-					Console.ReadKey();
+									Console.WriteLine($"{method.Name} Moded!");
+								}
+							}
+						}
+					}
+
+					if (status == GunaStatus.Cracked)
+					{
+						if (!Directory.Exists(newLocation))
+							Directory.CreateDirectory(newLocation);
+
+						module.Write($"{newLocation}\\{module.Name}");
+					}
+					else
+					{
+						Console.WriteLine("The guna is not valid!");
+					}
 				}
 			}
+			catch
+			{
+				Console.WriteLine("The file is not valid!");
+			}
+
+			Console.ReadKey();
 		}
-    }
+
+
+		static int CheckMethodsTypeSigCount(IList<MethodDef> methods, TypeSig typeSig)
+        {
+			int count = 0;
+			foreach (var method in methods)
+			{
+				if (method.ReturnType.FullName == typeSig.FullName)
+					count++;
+			}
+			return count;
+        }
+
+		static int CheckFieldsTypeSigCount(IList<FieldDef> fields, TypeSig typeSig)
+		{
+			int count = 0;
+			foreach (var field in fields)
+			{
+				if (field.FieldType.FullName == typeSig.FullName)
+					count++;
+			}
+			return count;
+		}
+
+		static int CheckFieldsTypeSigCount(IList<FieldDef> fields, TypeDef typeDef)
+		{
+			int count = 0;
+			foreach (var field in fields)
+			{
+				if (field.FieldType.FullName == typeDef.FullName)
+					count++;
+			}
+			return count;
+		}
+	}
 }
